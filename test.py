6e1cfd382c8a6c6a1f00aefc9997ea5e4854caea
daniel-frias <daniel@Dani-Linux>
2017-06-28 15:58:41 +0100

    bouncer test

--- test.py
+++ test.py
@@ -1,1 +1,36 @@
-#test pygame!
+#! /usr/bin/env python
+
+import pygame, time
+screenSize = (640, 400)
+screen = pygame.display.set_mode(screenSize)
+run = 1
+
+
+om = pygame.image.load('om.jpg').convert()
+omSize = (30,30)
+om = pygame.transform.scale(om, omSize)
+pos = [0,0]
+
+mov = [3,3]
+
+while run:
+  event = pygame.event.poll()
+  if event.type == pygame.QUIT:
+    run = 0
+
+  if pos[0] + mov[0] + omSize[0] > screenSize[0] or pos[0] + mov[0] < 0:
+    mov[0] *= -1
+
+  if pos[1] + mov[1] + omSize[1] > screenSize[1] or pos[1] + mov[1] < 0:
+    mov[1] *= -1
+  
+  pos[0] += mov[0]
+  pos[1] += mov[1]
+
+  screen.fill((0,0,0))
+  screen.blit(om, pos)
+
+  pygame.display.update()
+  
+
+  time.sleep(0.01)
