7237695732234ed8d738ab49a2719c8aeef2f655
daniel-frias <daniel@Dani-Linux>
2017-06-30 13:18:41 +0100

    Field: posicion en los elementos, pero hay que cambiarlo

--- field/field.py
+++ field/field.py
@@ -13,7 +13,7 @@ class Field:
   def createBackground(self):
     grass = Nothing(self.sizeTile, COLORGREEN, (0,0))
     back = pygame.Surface(SCREENSIZE)
-    print self.size
+    
     for y in range(0, self.size[1]):
       for x in range(0, self.size[0]):
         back.blit(grass.getTile(), (self.sizeTile[0]*x, self.sizeTile[0]*y))
@@ -25,7 +25,7 @@ class Field:
     for y in range(0, self.size[1]):
       for x in range(0, self.size[0]):
         if (random.randint(1,15) == 1):
-          self.field[x][y] = Bush(self.sizeTile, COLORGREEN, (0,0))
+          self.field[x][y] = Bush(self.sizeTile, COLORGREEN, (x,y))
 
 
     center = (self.size[0]/2, self.size[1]/2)
@@ -33,8 +33,8 @@ class Field:
       for y in range(center[1]-1, center[1]+1):
         self.field[x][y] = False
 
-    self.field[center[0]][center[1]] = Warehouse(self.sizeTile, COLORGREEN, (0,0))
-    self.field[center[0]][center[1]+1] = Chofli(self.sizeTile, COLORGREEN, (0,0))
+    self.field[center[0]][center[1]] = Warehouse(self.sizeTile, COLORGREEN, (center))
+    self.field[center[0]][center[1]+1] = Chofli(self.sizeTile, COLORGREEN, (center[0],center[1]+1))
 
         #else:
         #  self.field[x][y] = Nothing(self.sizeTile, COLORGREEN) 
@@ -113,7 +113,7 @@ mundo = Field(MUNDOSIZE, TILESIZE)
 frame = 0
 run = 1
 while run:
-  #print frame
+  print frame
   #EVENTOS
   event = pygame.event.poll()
   if event.type == pygame.QUIT:
